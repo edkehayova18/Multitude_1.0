788a690bd87bac9d869a0f09fbecffb44ad91b14
edkehayova18 <58166338+edkehayova18@users.noreply.github.com>
2020-10-18 21:54:21 +0300

    Update matitude.cpp

--- matitude/matitude.cpp
+++ matitude/matitude.cpp
@@ -4,16 +4,55 @@
 using namespace std;
 
 
+bool mainMenu() {
+	cout << "1. Section of two multitudes" << endl;
+	cout << "2. Unification of two multitudes" << endl;
+	cout << "3. Symetrical difference between two multitudes" << endl;
+	cout << "4. Difference between two multitudes" << endl;
+	cout << "5. Show if multitude A is a submultitude B " << endl;
+	cout << "6. Show if multitude A is equal to multitude B" << endl;
+	cout << "7. Show if multitude A have something it common with multitude B " << endl;
+	cout << "0. EXIT" << endl;
+	int option;
+	cin >> option;
+	switch (option)
+	{
+	case 0:
+		return false;
+		break;
+	case 1:
+		return true;
+		break;
+	case 2:
+		return true;
+		break;
+	case 3:
+		return true;
+		break;
+	case 4:
+		return true;
+		break;
+	case 5:
+		return true;
+	
+		break;
+	case 6:
+		return true;
+		break;
+	case 7:
+		return true;
+		break;
+	}
 
-//bool mainMenu() {
-
-//}
+}
 
 int main()
 {
 	int sizeOfMaltitudeA, sizeOfMaltitudeB;
 	int* maltitudeA, * maltitudeB;
 
+	bool exit= true;
+	
 	cin >> sizeOfMaltitudeA;
 
 	maltitudeA = new int[sizeOfMaltitudeA];
@@ -39,4 +78,7 @@ int main()
 
 	delete[]maltitudeA;
 	delete[]maltitudeB;
+	
+	do {exit=mainMenu()} while (exit!= false)
+	
 }
